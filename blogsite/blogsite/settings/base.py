90369d26e7781279cfa03a1ebe264f31eecd5117
gregname <gregname@reliant.dreamhost.com>
2019-11-23 02:01:30 -0800

    reverted to db.sqlite3

--- blogsite/blogsite/settings/base.py
+++ blogsite/blogsite/settings/base.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db-copy.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

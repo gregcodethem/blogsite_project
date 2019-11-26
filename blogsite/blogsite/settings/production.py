b4539946bb3a9241e2aaff78d6d87f5637011384
gregstage <gregstage@reliant.dreamhost.com>
2019-11-26 03:22:59 -0800

    staging settings changes

--- blogsite/blogsite/settings/production.py
+++ blogsite/blogsite/settings/production.py
@@ -4,9 +4,9 @@ from .env import SECRET_KEY
 DEBUG = False
 #DEBUG = True
 
-STATIC_ROOT = '/home/gregname/gregshepley.com/public/static'
+STATIC_ROOT = '/home/gregstage/staging.gregshepley.com/public/static'
 
-MEDIA_ROOT = '/home/gregname/gregshepley.com/public/media'
+MEDIA_ROOT = '/home/gregstage/staging.gregshepley.com/public/media'
 
 try:
     from .local import *
@@ -18,7 +18,7 @@ try:
 except ImportError:
     pass
 
-ALLOWED_HOSTS = ['.gregshepley.com']
+ALLOWED_HOSTS = ['.gregshepley.com','.staging.gregshepley.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 

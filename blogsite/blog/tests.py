e370f5b0676a09e6b3a3dc011b9eac4874637636
Greg Shepley <gregshepley@yahoo.co.uk>
2019-11-27 11:10:15 +0000

    first part of test

--- blogsite/blog/tests.py
+++ blogsite/blog/tests.py
@@ -1,3 +1,11 @@
+import unittest
 from django.test import TestCase
 
+from blogsite.settings.production import STATIC_ROOT
+
 # Create your tests here.
+class SettingsTest(TestCase):
+
+	def static_root_correct_for_live(self):
+		STATIC_ROOT_LIVE = '/home/gregname/gregshepley.com/public/static'
+		self.assertEqual(STATIC_ROOT, STATIC_ROOT_LIVE)

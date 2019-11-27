fc2b3663a9a03626a41b66cb7de7ef6ec8ea3345
Greg Shepley <gregshepley@yahoo.co.uk>
2019-11-27 12:18:30 +0000

    smoking test added and test name changed

--- blogsite/blog/tests.py
+++ blogsite/blog/tests.py
@@ -6,6 +6,9 @@ from blogsite.settings.production import STATIC_ROOT
 # Create your tests here.
 class SettingsTest(TestCase):
 
-	def static_root_correct_for_live(self):
+	def static_root_correct_for_live_test(self):
 		STATIC_ROOT_LIVE = '/home/gregname/gregshepley.com/public/static'
 		self.assertEqual(STATIC_ROOT, STATIC_ROOT_LIVE)
+
+	def smoking_test():
+		self.assertEqual(1,2)

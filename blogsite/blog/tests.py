27500edf0a2f655bb385969c79566ebd62558938
Greg Shepley <gregshepley@yahoo.co.uk>
2019-11-27 20:34:31 +0000

    full test diff roots

--- blogsite/blog/tests.py
+++ blogsite/blog/tests.py
@@ -28,11 +28,20 @@ class SettingsTest(TestCase):
     def test_static_root_correct_for_production(self):
 
         STATIC_ROOT_LIVE = '/home/gregname/gregshepley.com/public/static'
+        STATIC_ROOT_STAGE = '/home/gregstage/staging.gregshepley.com/public/static'
+        STATIC_ROOT_LOCAL = '/home/gregshepley/code/projects/blogsite_project/blogsite'
+        BASE_URL_LOCAL = '/Users/gregshepley/code/projects/blogsite_project/blogsite'
+        BASE_URL_LIVE = '/home/gregname/gregshepley.com/blogsite_project'
+        BASE_URL_STAGING = '/home/gregstage/staging.gregshepley.com/blogsite_project/blogsite'
 
         self.assertEqual(
-            static_root(BASE_DIR),
+            static_root(BASE_DIR_LIVE),
             STATIC_ROOT_LIVE
         )
+        self.assertEqual(
+        	static_root(BASE_URL_STAGING),
+        	STATIC_ROOT_STAGE
+        	)
 
     def test_static_root_correct_for_staging(self):
         STATIC_ROOT_STAGE = '/home/gregstage/staging.gregshepley.com/public/static'
